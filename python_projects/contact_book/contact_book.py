6f2d1f1b68aa4db5acffa834e4aaeb233e9d8430
Behruz-Hojaniyazow <behruzhojaniyazow@gmail.com>
2026-06-01 22:58:10 +0500

    refactor: improve contact management and error handling
    - renamed load_contact() to load_contacts()
    - added JSONDecodeError handling
    - added logging for invalid JSON files
    - fixed delete_contact() loop behavior
    - improved contact deletion flow
    - cleaned up function structure and validations
    - updated contact persistence logic

--- python_projects/contact_book/contact_book.py
+++ python_projects/contact_book/contact_book.py
@@ -1,12 +1,66 @@
 import sys
+import json
+import logging
+
+FILE_NAME = 'contacts_info.json'
+
+logging.basicConfig(level=logging.ERROR)
+
+def load_contacts():
+  """
+  Reads contacts from a JSON file.
+
+  If the file does not exist:
+  - returns an empty list
+
+  If the file exists:
+  - returns the data in the JSON
+  """
+  
+  try:
+    
+    with open(FILE_NAME, 'r', encoding='utf-8') as file:
+      contacts = json.load(file)
+      
+      return contacts
+      
+  except FileNotFoundError:
+    
+    return []
+
+  except json.JSONDecodeError as e:
+    logging.error(f"\nInvalid JSON format - {e}")
+    
+    return []
+    
+  
+    
+def save_contacts(contacts):
+  """Save contacts to a json file"""
+  
+  try:
+    
+    with open (FILE_NAME, 'w', encoding = 'utf-8') as file:
+      json.dump(
+        contacts,
+        file, 
+        indent=4,
+        ensure_ascii=False
+      )
+      
+  except IOError as e:
+    print(f"File error - {e}")
+    
+  except Exception as e:
+    print(f"An error occured - {e}")
       
 def add_contact():
   """Collect contact information from the user and store it in a list"""
   
-  contacts = []
-  order = 1
+  contacts = load_contacts()
+  order = len(contacts) + 1
   
-  print("\nType 'stop' to finish!")
+  print("\nType 'stop' to stop adding contacts!")
   # Start collecting contact information
   while True:
     print(f"\n{order}-Enter information about a person!")
@@ -52,14 +106,15 @@ def add_contact():
     
     # Save contact
     contacts.append(contact)
+    save_contacts(contacts)
     order += 1
     
     print("✅️\nContact saved successfully!")
   
-  return contacts
-  
-def show_contacts(contacts):
+def show_contacts():
   """Display all saved contacts in a formatted table"""
+  
+  contacts = load_contacts()
   
   # Return early if there are no saved contacts
   if not contacts:
@@ -68,17 +123,20 @@ def show_contacts(contacts):
   
   # Display All Contacts
   print('\n' + '=' * 43)
-  print(f'{'Name':<15} | {'Phone Number':<20}')
+  print(f" {'Name':<17} | {'Phone Number':<20}")
   print('-' * 43)
-  for contact in contacts:
+  for index, contact in enumerate(contacts, start=1):
     print(
+      f"{index}. "
       f"{contact['name'].title():<15} | "
       f"+{contact['phone']:<20}"
     )
   print("=" * 43)
   
-def search_contact(contacts):
+def search_contact():
   """Function that searches a contact from the list"""
+  
+  contacts = load_contacts()
   
   # Return early if there are no saved contacts
   if not contacts:
@@ -113,8 +171,10 @@ def search_contact(contacts):
     if not found:
       print(f"\nNo contacts found named {user_input.title()}")
       
-def delete_contact(contacts):
+def delete_contact():
   """Delete a contact from the contact list"""
+  
+  contacts = load_contacts()
   
   # Return early if there are no saved contacts
   if not contacts:
@@ -146,44 +206,24 @@ def delete_contact(contacts):
             contacts.remove(contact)
             print(f"\n{contact['name'].title()} was deleted successfully!")
             
+            save_contacts(contacts)
+            
             deleted = True
-            return
+            break
         
           elif confirm in ('no', 'n'):
             print(f"\n{contact['name'].title()} was not deleted!")
             
-            return
+            deleted = True
+            break
         
           else:
             print("\nPlease type yes or no!")
-        
+                                   
+                                  
+        break                      
     if not deleted:
       print(f"\nNo contact found named {user_input.title()}")
-
-def save_contacts(contacts):
-  """Save contacts to a text file"""
-  
-  filename = 'contacts_info.txt'
-  
-  if not contacts:
-    print("\n📂 Not contacts info found to write!")
-    return
-  
-  try:
-    
-    with open (filename, 'w', encoding = 'utf-8') as f:
-      
-      for contact in contacts:
-        formatted_contact = f"{contact['name'].title():<15} | {contact['phone']:<20}\n"
-        f.write(formatted_contact)
-        
-      print("\nContacts Saved to the file successfully! ✅️")
-      
-  except IOError as e:
-    print(f"File error - {e}")
-    
-  except Exception as e:
-    print(f"An error occured - {e}")
     
 def exit_app():
   """Exit the application gracefully"""
@@ -193,15 +233,12 @@ def exit_app():
 
 def main():
   
-  contacts = []
-  
   menu_actions = {
     '1' : 'Add Contact',
     '2' : 'Show Contacts',
     '3' : 'Search Contacts',
     '4' : 'Delete Contacts',
-    '5' : 'Save Contacts to the File',
-    '6' : 'Exit app'
+    '5' : 'Exit app'
   }
   
   while True:
@@ -215,26 +252,22 @@ def main():
     choice = input("\nChoose an action: ").strip()
     
     if choice == '1':
-      new_contacts = add_contact()
-      contacts.extend(new_contacts)
+      add_contact()
       
     elif choice == '2':
-      show_contacts(contacts)
+      show_contacts()
       
     elif choice == '3':
-      search_contact(contacts)
+      search_contact()
       
     elif choice == '4':
-      delete_contact(contacts)
+      delete_contact()
       
     elif choice == '5':
-      save_contacts(contacts)
-      
-    elif choice == '6':
       exit_app()
       
     else:
-      print("\n❌️Invalid choice, Please choose (1 to 6)")
+      print("\n❌️Invalid choice, Please choose (1 to 5)")
       
 if __name__ == '__main__':
   main()
