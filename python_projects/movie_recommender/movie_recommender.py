75fe213c0278af92d0be37f26c7024a30a24113b
Behruz-Hojaniyazow <behruzhojaniyazow@gmail.com>
2026-06-03 22:06:48 +0500

    fix: fix initialization crash and optimize movie recommender logic
    - Add missing return statement in `load_movies()` under FileNotFoundError block
    - Standardize code by adding missing logger warning states for duplicate entries
    - Fix movie dictionary value access and typos in default movie names
    - Enhance user experience inside command-line menu loops

--- python_projects/movie_recommender/movie_recommender.py
+++ python_projects/movie_recommender/movie_recommender.py
@@ -1,20 +1,103 @@
 import random
 import sys
+import json
+import logging
 
+FILE_NAME = 'movies.json'
+LOG_FILE = 'movies_app.log'
+
+# creating a log
+logger = logging.getLogger('MovieRecommender')
+logger.setLevel(logging.DEBUG) # Accepts logs of all level
+
+# 1.Handler for writing to a file(ALL ERROR and CRITICAL ERRORS are written to a file for analysis)
+file_handler = logging.FileHandler(LOG_FILE, encoding='utf-8')
+file_handler.setLevel(logging.ERROR)
+file_formatter = logging.Formatter(
+  '[%(asctime)s] %(levelname)s [%(name)s:%(filename)s:%(lineno)d] - %(message)s'
+)
+file_handler.setFormatter(file_formatter)
+
+# 2.Handler for output to the console (Only for the user or programmer can see on the screen)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_formatter = logging.Formatter('%(levelname)s: %(message)s')
+console_handler.setFormatter(console_formatter)
+
+# Add handlers to the logger
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+
+def load_movies():
+  """
+  Reads movies from a JSON file.
+  
+  If the file does not exist:
+  - returns an empty dict
+  
+  If the file exists:
+  - returns the data in the JSON file
+  """
+  
+  try:
+    with open (FILE_NAME, 'r', encoding='utf-8') as file:
+      movies = json.load(file)
+      
+      return movies
+      
+  except FileNotFoundError:
+    # It is normal for file not to exist (when opened for the first time) this can be done with a small warning (DEBUG)
+    logger.debug(f"'{FILE_NAME}' not found, Initializing with default movies")
+    default_movies = create_movies()
+    save_movies(default_movies)
+    return default_movies
+    
+  except json.JSONDecodeError as e:
+    # JSON STRUCTURE is corrupted - this is a serious error!
+    logger.exception(f"Invalid JSON format - {e}")
+    
+    return {}
+    
+  except Exception as e:
+    logger.exception(f"Unexpected error occured (load_movies) - {e}")
+    
+    return {}
+    
+def save_movies(movies):
+  """Save movies to a JSON file"""
+  
+  try:
+    
+    with open(FILE_NAME, 'w', encoding='utf-8') as file:
+      json.dump(
+        movies,
+        file,
+        indent=4,
+        ensure_ascii=False
+      )
+      
+  except IOError as e:
+    logger.exception(f"File error - {e}")
+    
+  except Exception as e:
+    logger.exception(f"An error occured while saving movies - {e}")
+    
 def create_movies():
   """Function that collects movies into a dict"""
   movies = {
     "action": ["John Wick", "Extraction", "Mad Max", "The Dark Knight", "Inception"],
     "comedy": ["Mr Bean", "Home Alone", "The Mask", "Rush Hour", "Hangover"],
     "horror" : ["It" , "Conjuring", "Get Out", "Insidious", "A Quiet Place"],
-    "drama" : ["The Shawshank Redemption", "Forrest Gump", "The GodFather", "The Pursuit In Happyness", "Interstellar"],
+    "drama" : ["The Shawshank Redemption", "Forrest Gump", "The GodFather", "The Pursuit of Happyness", "Interstellar"],
     "sci-fi" : ["Avatar", "The Matrix", "Dune", "Blade Runner 2049", "Tenet"]
   }
   
   return movies
 
-def recommend_movie(movies):
+def recommend_movie():
   """Function that recommends a movie depending on a users genre"""
+  
+  movies = load_movies()
   
   if not movies:
     print("\nNo new movies to watch!")
@@ -50,14 +133,15 @@ def recommend_movie(movies):
       print(f"\n🍿 Recommended movie for you: {chosen_movie.title()} ({selected_genre.title()}) genre!")
       print("Enjoy your view!")
       
+      logger.info(f"Successfully recommended '{chosen_movie}' from '{selected_genre}' genre to user")
+      
     else:
       print("\nInvalid choice, Please choose a valid option!")
-      
-#all_movies = create_movies()
-#recommend_movie(all_movies)
 
-def add_movie(movies):
+def add_movie():
   """Function that adds movies into the existing dictionary"""
+  
+  movies = load_movies()
   
   while True:
     print("\nType 'stop' to stop adding movies")
@@ -91,6 +175,7 @@ def add_movie(movies):
         if selected_genre.lower() in [g.lower() for g in movies.keys()]:
           print("\nThis genre already exists")
           print("Please enter a new genre!")
+          logger.warning(f"Failed to add genre: '{selected_genre.title()}' already exists")
           continue
         selected_genre = selected_genre.lower()
       elif new_genre.lower() in ('no', 'n'):
@@ -122,19 +207,23 @@ def add_movie(movies):
       
       if movie_name.lower() in all_existing_movies:
         print("\nThis movie is already in the database!")
+        logger.warning(f"Failed to add movie: '{movie_name.title()}' already exists in database")
         continue
   
       if new_genre.lower() in ('yes', 'y'):
         movies[selected_genre] = [movie_name]
+        save_movies(movies)
     
       else:
         movies[selected_genre].append(movie_name)
+        save_movies(movies)
       print(f"✅️ Great! The movie '{movie_name.title()}' has been successfully added!")
-#new_genres = create_movies()
-#add_movie(new_genres)
+      logger.info(f"Successfully added '{movie_name}' movie")
 
-def search_movie(movies):
+def search_movie():
   """Function that finds a movie entered by the user"""
+  
+  movies = load_movies()
   
   if not movies:
     print("\nNo movie found, Database is empty!")
@@ -169,21 +258,22 @@ def search_movie(movies):
       print(f"\nYes, This movie was found")
       print(f"Genre: {found_genre.title()}")
       print(f"Movie title: {found_movie.title()}")
+      logger.info(f"Successfully found movie: {found_movie.title()} from {found_genre.title()} genre")
     else:
       print(f"\nUnfortunately, {user_input.title()} movie not found")
+      logger.info(f"Search failed, No movie found named {user_input.title()}")
       
-#search_movies = create_movie()
-#search_movie(search_movies)
-
-def delete_movie(movies):
+def delete_movie():
   """Function that deletes movies entered by the user"""
+  
+  movies = load_movies()
   
   if not movies:
     print("\nThere's no film to delete, Database is empty")
     return
   
   while True:
-    print("\nType stop to stop deleting")
+    print("\nType 'stop' to stop deleting")
     user_input = input("\nWhich movie would you like to delete: ").strip()
     
     if not user_input:
@@ -210,7 +300,9 @@ def delete_movie(movies):
         
             if movie_delete.lower() in ('yes', 'y'):
               movies_list.remove(movie)
+              save_movies(movies)
               print(f"\n{movie.title()} has been successfully deleted!")
+              logger.info(f"Successfully deleted {movie.title()}")
               break
             
             elif movie_delete.lower() in ('no', 'n'):
@@ -227,12 +319,12 @@ def delete_movie(movies):
               
     if not movie_found:
       print(f"\nNo movie found named {user_input.title()}!")
+      logger.warning(f"Delete failed: Movie '{user_input.title()}' not found")
 
-#remove_movie = create_movies()
-#delete_movie(remove_movie)
-
-def show_movies(movies):
+def show_movies():
   """Function that shows all existing movies"""
+  
+  movies = load_movies()
   
   if not movies:
     print("\nNo movies found to show")
@@ -268,8 +360,6 @@ def exit_app():
   
 def main():
   
-  kryos_movies = create_movies()
-  
   menu_actions = {
     '1' : 'recommend movie',
     '2' : 'add movie',
@@ -278,37 +368,47 @@ def main():
     '5' : 'show movies',
     '6' : 'exit app'
   }
-  while True:
-    print("\n" + "=" * 35)
-    print("Welcome to Kryos Movie Program")
-    print("=" * 35)
-    for key, value in menu_actions.items():
-      print(f"{key} -> {value.title()}")
+  
+  try:
+    while True:
+      print("\n" + "=" * 35)
+      print("Welcome to Kryos Movie Program")
+      print("=" * 35)
+      for key, value in menu_actions.items():
+        print(f"{key} -> {value.title()}")
       
-    user_choice = input("Choose an action: ").strip()
+      user_choice = input("Choose an action: ").strip()
     
-    if user_choice == '1':
-      recommend_movie(kryos_movies)
+      if user_choice == '1':
+        recommend_movie()
       
-    elif user_choice == '2':
-      add_movie(kryos_movies)
+      elif user_choice == '2':
+        add_movie()
       
-    elif user_choice == '3':
-      search_movie(kryos_movies)
+      elif user_choice == '3':
+        search_movie()
       
-    elif user_choice == '4':
-      delete_movie(kryos_movies)
+      elif user_choice == '4':
+        delete_movie()
     
-    elif user_choice == '5':
-      show_movies(kryos_movies)
+      elif user_choice == '5':
+        show_movies()
       
-    elif user_choice == '6':
-      exit_app()
+      elif user_choice == '6':
+        exit_app()
       
-    else:
-      print("\nInvalid choice, Please choose only (1 to 6)")
+      else:
+        print("\nInvalid choice, Please choose only (1 to 6)")
       
+  except KeyboardInterrupt:
+    # close gracefully without throwing an error when the user presses Ctrl+C
+    print("\n\nProject was stopped by the user")
+    sys.exit(0)
     
+  except Exception as e:
+    # Any unexpected critical error in the program goes here
+    logger.critical(f"A critical system error has occured and the program has stopped! Global Error - {e}", exc_info=True)
+    sys.exit(1)
       
 if __name__ == '__main__':
   main()
